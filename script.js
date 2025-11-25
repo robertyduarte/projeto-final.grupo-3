d87acb611f74f7caf6b599ad36359f394b878467
nathandeoliveiralisboa-svg <nathandeoliveiralisboa@gmail.com>
2025-11-24 23:13:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -47,3 +47,13 @@ document.addEventListener('DOMContentLoaded', function() {
             });
     });
 });
+
+// TESTE NATHAN HEADER
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
